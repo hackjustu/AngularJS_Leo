a232ccf72bf5eccfb17b09286f3690fde7c41ffa
hackjustu <wenzhe89929@ucla.edu>
2016-04-19 00:19:31 -0700

    Refactor the weatherAPI into a service

--- services.js
+++ services.js
@@ -4,3 +4,13 @@ weatherApp.service('cityService', function() {
     this.city = 'New York, NY';
     
 });
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+    
+    this.GetWeather = function(city, days) {
+        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=6f169dc7575003b0e9803c1be4209fd2", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+        
+        return weatherAPI.get({ q: city, cnt: days });
+    };
+    
+}]);
